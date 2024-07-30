d84d50c6e7cad440e18c62434ef6eb4b176cf320
HP <HP@DESKTOP-2L57IV8>
2024-07-30 22:07:03 +0530

    Add project files.

---
+++ Pizza.cs
@@ -0,0 +1,73 @@
+﻿public class Pizza
+{
+    // Private instance variables
+    private string size;
+    private int cheeseToppings;
+    private int pepperoniToppings;
+    private int hamToppings;
+
+    // Constructor(s) that set all of the instance variables
+    public Pizza(string size, int cheeseToppings, int pepperoniToppings, int hamToppings)
+    {
+        this.size = size;
+        this.cheeseToppings = cheeseToppings;
+        this.pepperoniToppings = pepperoniToppings;
+        this.hamToppings = hamToppings;
+    }
+
+    // Properties using get and set methods
+    public string Size
+    {
+        get { return size; }
+        set { size = value; }
+    }
+
+    public int CheeseToppings
+    {
+        get { return cheeseToppings; }
+        set { cheeseToppings = value; }
+    }
+
+    public int PepperoniToppings
+    {
+        get { return pepperoniToppings; }
+        set { pepperoniToppings = value; }
+    }
+
+    public int HamToppings
+    {
+        get { return hamToppings; }
+        set { hamToppings = value; }
+    }
+
+    // Method to calculate the cost of the pizza
+    public double CalcCost()
+    {
+        double cost = 0;
+
+        switch (size.ToLower())
+        {
+            case "small":
+                cost = 10 + 2 * (cheeseToppings + pepperoniToppings + hamToppings);
+                break;
+            case "medium":
+                cost = 12 + 2 * (cheeseToppings + pepperoniToppings + hamToppings);
+                break;
+            case "large":
+                cost = 14 + 2 * (cheeseToppings + pepperoniToppings + hamToppings);
+                break;
+            default:
+                Console.WriteLine("Invalid size specified. Using default cost for small pizza.");
+                cost = 10 + 2 * (cheeseToppings + pepperoniToppings + hamToppings);
+                break;
+        }
+
+        return cost;
+    }
+
+    // Method to get description of the pizza
+    public string GetDescription()
+    {
+        return $"Pizza Size: {size}, Cheese Toppings: {cheeseToppings}, Pepperoni Toppings: {pepperoniToppings}, Ham Toppings: {hamToppings}";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assignment6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating instances of Pizza
+            Pizza pizza1 = new Pizza("small", 1, 2, 1);
+            Pizza pizza2 = new Pizza("medium", 2, 3, 0);
+
+            // Displaying pizza details
+            Console.WriteLine("Pizza 1:");
+            Console.WriteLine(pizza1.GetDescription());
+            Console.WriteLine($"Cost: ${pizza1.CalcCost()}");
+
+            Console.WriteLine("\nPizza 2:");
+            Console.WriteLine(pizza2.GetDescription());
+            Console.WriteLine($"Cost: ${pizza2.CalcCost()}");
+
+            // Example of updating properties
+            pizza2.Size = "large";
+            pizza2.CheeseToppings = 4;
+
+            Console.WriteLine("\nUpdated Pizza 2:");
+            Console.WriteLine(pizza2.GetDescription());
+            Console.WriteLine($"Cost: ${pizza2.CalcCost()}");
+
+            // Storing records in a List
+            List<Pizza> pizzas = new List<Pizza>();
+            pizzas.Add(pizza1);
+            pizzas.Add(pizza2);
+
+            // Displaying all pizzas in the list
+            Console.WriteLine("\nAll Pizzas:");
+            foreach (var pizza in pizzas)
+            {
+                Console.WriteLine(pizza.GetDescription());
+                Console.WriteLine($"Cost: ${pizza.CalcCost()}");
+                Console.WriteLine();
+            }
+
+            // Keep console window open until a key is pressed
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
